2f98bc99584dc597097c050436a600165756a2c5
Bongz Podolski <ntshingilab@outlook.com>
2022-10-13 09:42:31 -0300

    stats added

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,18 @@ window.addEventListener("scroll", function () {
     goTopBtn.classList.remove("active");
   }
 });
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 300;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
